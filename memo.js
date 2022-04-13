60421babcffc48bdb307c4e97309269183fbfb09
shusuke919 <sh091965@gmail.com>
2022-04-13 17:30:26 +0900

    メモ帳完成

--- memo.js
+++ memo.js
@@ -3,7 +3,17 @@ $("main").fadeIn(1000);
 $("#btn_a").on("click",function(){
 const memo =$("#text_area").val();
 localStorage.setItem("memo",memo);
-alert("で行こう");
+alert("保存しました");
+});
 
+$("#btn_b").on("click",function(){
+localStorage.removeItem("memo");
+alert("セーブしました")
+$("#text_area").val("")
+});
 
-});e
+if(localStorage.getItem("memo")){
+const value = localStorage.getItem("memo");
+$("#text_area").val(value);
+
+};
